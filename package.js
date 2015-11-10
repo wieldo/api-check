92e65f08816290fa9564494d5dc72f8c48fbb6f5
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-11-10 10:47:55 +0100

    Add documentation and git url of package

--- package.js
+++ package.js
@@ -5,7 +5,10 @@ var server = 'server';
 Package.describe({
     name: "wieldo:api-check",
     summary: "Validate the api to your functions to help people use them correctly. This is pretty much React's propTypes without React.",
-    version: "7.5.5"
+    version: "7.5.5",
+
+    documentation: 'README.md',
+    git: 'https://github.com/wieldo/api-check.git'
 });
 
 Package.onUse(function (api) {
